c83de19f216f5984f56219389ef5fdbf24e42e3c
mratkovic <marko.ratkovic93@gmail.com>
2017-01-11 12:23:13 +0100

    Lab2 initial commit, added template

---
+++ lab2_ws/chat/consumers.py
@@ -0,0 +1,73 @@
+import re
+import json
+import logging
+from channels import Group
+from channels.sessions import channel_session
+from .models import Room
+
+
+@channel_session
+def ws_connect(message):
+    try:
+        prefix, label = message['path'].strip('/').split('/')
+        if prefix != 'chat':
+            logging.debug('invalid ws path=%s', message['path'])
+            return
+        room = Room.objects.get(label=label)
+
+    except ValueError:
+        logging.debug('invalid ws path=%s', message['path'])
+        return
+
+    except Room.DoesNotExist:
+        logging.debug('ws room does not exist label=%s', label)
+        return
+
+        logging.debug('chat connect room=%s client=%s:%s', room.label, message['client'][0], message['client'][1])
+
+    # TODO: mozda treba jos  channel_layer=message.channel_layer
+    Group(Room.room_id_from_label(label), channel_layer=message.channel_layer).add(message.reply_channel)
+    message.channel_session['room'] = room.label
+
+
+@channel_session
+def ws_receive(message):
+    # Look up the room from the channel session, bailing if it doesn't exist
+    try:
+        label = message.channel_session['room']
+        room = Room.objects.get(label=label)
+
+    except KeyError:
+        logging.debug('no room in channel_session')
+        return
+
+    except Room.DoesNotExist:
+        logging.debug('recieved message, but room does not exist label=%s', label)
+        return
+
+    try:
+        data = json.loads(message['text'])
+
+    except ValueError:
+        logging.debug("ws message isn't json text=%s", text)
+        return
+
+    if set(data.keys()) != set(('handle', 'message')):
+        logging.debug("ws message unexpected format data=%s", data)
+        return
+
+    if data:
+        logging.debug('chat message room=%s handle=%s message=%s', room.label, data['handle'], data['message'])
+        m = room.messages.create(**data)
+        Group(Room.room_id_from_label(label), channel_layer=message.channel_layer).send({'text': json.dumps(m.as_dict())})
+
+
+@channel_session
+def ws_disconnect(message):
+    try:
+        label = message.channel_session['room']
+        _ = Room.objects.get(label=label)
+        Group(Room.room_id_from_label(label), channel_layer=message.channel_layer).discard(message.reply_channel)
+
+    except (KeyError, Room.DoesNotExist):
+        pass
---
+++ lab2_ws/chat/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from django.db import models
+from django.utils import timezone
+
+
+class Room(models.Model):
+    name = models.TextField()
+    label = models.SlugField(unique=True)
+
+    def __unicode__(self):
+        return self.label
+
+    @staticmethod
+    def room_id_from_label(label):
+        return 'chat-' + label
+
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, related_name='messages')
+    handle = models.TextField()
+    message = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now, db_index=True)
+
+    def __unicode__(self):
+        return '[{timestamp}]\t{handle}\t{message}'.format(**self.as_dict())
+
+    @property
+    def formatted_timestamp(self):
+        return self.timestamp.strftime('%b %-d %-I:%M %p')
+
+    def as_dict(self):
+        return {'handle': self.handle, 'message': self.message, 'timestamp': self.formatted_timestamp}
