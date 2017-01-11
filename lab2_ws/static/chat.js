3616740d033c0b93e87269ac09cf3bfa0172b56f
mratkovic <marko.ratkovic93@gmail.com>
2017-01-11 22:09:02 +0100

    Added bootstrap

--- lab2_ws/static/chat.js
+++ lab2_ws/static/chat.js
@@ -3,22 +3,21 @@ $(function() {
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
     var chatsock = new WebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
 
+    var objDiv = $("#chat-list");
+    objDiv.scrollTop(objDiv.prop("scrollHeight"));
+
     chatsock.onmessage = function(message) {
-        var data = JSON.parse(message.data);
-        var chat = $("#chat")
-        var ele = $('<tr></tr>')
+        var data = JSON.parse(message.data)
+        var chat = $("#chat-element").first().clone().appendTo("#chat-element-list").removeClass('hidden');
+        var body = chat.find("#chat-element-body")
+        body.text(data.message)
 
-        ele.append(
-            $("<td></td>").text(data.timestamp)
-        )
-        ele.append(
-            $("<td></td>").text(data.handle)
-        )
-        ele.append(
-            $("<td></td>").text(data.message)
-        )
+        var details = chat.find("#chat-element-details")
+        details.text(data.handle + " " + data.timestamp)
+        data.remove
 
-        chat.append(ele)
+        var objDiv = $("#chat-list");
+        objDiv.scrollTop(objDiv.prop("scrollHeight"));
     };
 
     $("#chatform").on("submit", function(event) {
