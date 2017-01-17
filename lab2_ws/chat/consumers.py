2930b249994619c2f17493544db2c0d471ca6cbc
mratkovic <marko.ratkovic93@gmail.com>
2017-01-17 18:24:58 +0100

    Removed irelevant error checking

--- lab2_ws/chat/consumers.py
+++ lab2_ws/chat/consumers.py
@@ -7,54 +7,18 @@ from .models import Room
 
 @channel_session
 def ws_connect(message):
-    try:
-        prefix, label = message['path'].strip('/').split('/')
-        if prefix != 'chat':
-            logging.debug('invalid ws path=%s', message['path'])
-            return
-        room = Room.objects.get(label=label)
-
-    except ValueError:
-        logging.debug('invalid ws path=%s', message['path'])
-        return
-
-    except Room.DoesNotExist:
-        logging.debug('ws room does not exist label=%s', label)
-        return
-
-    logging.debug('chat connect room=%s client=%s:%s', room.label, message['client'][0], message['client'][1])
+    prefix, label = message['path'].strip('/').split('/')
+    room = Room.objects.get(label=label)
     Group(Room.room_id_from_label(label)).add(message.reply_channel)
     message.channel_session['room'] = room.label
 
 
 @channel_session
 def ws_receive(message):
-    # Look up the room from the channel session, bailing if it doesn't exist
-    try:
-        label = message.channel_session['room']
-        room = Room.objects.get(label=label)
-
-    except KeyError:
-        logging.debug('no room in channel_session')
-        return
-
-    except Room.DoesNotExist:
-        logging.debug('recieved message, but room does not exist label=%s', label)
-        return
-
-    try:
-        data = json.loads(message['text'])
-
-    except ValueError:
-        logging.debug("ws message isn't json text=%s", message['text'])
-        return
-
-    if set(data.keys()) != set(('handle', 'message')):
-        logging.debug("ws message unexpected format data=%s", data)
-        return
-
+    label = message.channel_session['room']
+    room = Room.objects.get(label=label)
+    data = json.loads(message['text'])
     if data:
-        logging.debug('chat message room=%s handle=%s message=%s', room.label, data['handle'], data['message'])
         m = room.messages.create(**data)
         Group(Room.room_id_from_label(label)).send({'text': json.dumps(m.as_dict())})
 
